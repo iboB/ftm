a192cc7b332f91addbc1365c006952208ab54c44
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-07-07 11:56:27 +0300

    More types

--- code/ftm/types.hpp
+++ code/ftm/types.hpp
@@ -19,10 +19,13 @@ using sec_i64 = std::chrono::duration<int64_t>;
 
 using ms_t = std::chrono::milliseconds;
 using ms_i64 = std::chrono::duration<int64_t, std::milli>;
+using ms_i32 = std::chrono::duration<int32_t, std::milli>;
 
 using us_t = std::chrono::microseconds;
 using us_i64 = std::chrono::duration<int64_t, std::micro>;
+using us_i32 = std::chrono::duration<int32_t, std::micro>;
 
 using ns_t = std::chrono::nanoseconds;
 using ns_i64 = std::chrono::duration<int64_t, std::nano>;
+using ns_i32 = std::chrono::duration<int32_t, std::nano>;
 }
